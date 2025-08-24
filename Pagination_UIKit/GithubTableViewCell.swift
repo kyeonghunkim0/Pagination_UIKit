0f18c71ac8f20888b8040dfd206d75b29a6f995f
kyeonghumkim0 <kimkhuna1@gmail.com>
2025-08-24 12:27:20 +0900

    UI 구성

---
+++ Pagination_UIKit/GithubTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  GithubTableViewCell.swift
+//  Pagination_UIKit
+//
+//  Created by 김경훈 on 8/24/25.
+//
+
+import UIKit
+
+final class GithubTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "GithubTableViewCell"
+    
+    private let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(systemName: "person")
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .medium)
+        label.text = "mojombo"
+        return label
+    }()
+    
+    private let linkLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.textColor = .secondaryLabel
+        label.text = "https://github.com/mojombo"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    func setupUI() {
+        [thumbnailImageView, nameLabel, linkLabel].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        selectionStyle = .none
+        
+        let d: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            thumbnailImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: d),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: d),
+            thumbnailImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -d),
+            thumbnailImageView.widthAnchor.constraint(equalToConstant: 90),
+            
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: d),
+            nameLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: d * 2),
+            nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -d),
+            
+            linkLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: d / 2),
+            linkLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: d * 2),
+            linkLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -d)
+        ])
+    }
+}
--- Pagination_UIKit/GithubViewController.swift
+++ Pagination_UIKit/GithubViewController.swift
@@ -7,9 +7,42 @@
 
 import UIKit
 
-class GithubViewController: UIViewController {
+final class GithubViewController: UIViewController {
+    
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(GithubTableViewCell.self,
+                           forCellReuseIdentifier: GithubTableViewCell.identifier)
+        tableView.rowHeight = 110
+        tableView.estimatedRowHeight = 110
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension GithubViewController: UITableViewDelegate {
+}
+
+extension GithubViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: GithubTableViewCell.identifier, for: indexPath)
+        return cell
     }
 }
