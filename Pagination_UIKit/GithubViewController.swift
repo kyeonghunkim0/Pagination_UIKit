1f2e6af589c45ab1cd3b28cb363092ce29a744de
kyeonghumkim0 <kimkhuna1@gmail.com>
2025-08-24 11:57:13 +0900

    최초 커밋

---
+++ Pagination_UIKit/GithubViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GithubViewController.swift
+//  Pagination_UIKit
+//
+//  Created by 김경훈 on 8/24/25.
+//
+
+import UIKit
+
+class GithubViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Pagination_UIKit/SceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  Pagination_UIKit
+//
+//  Created by 김경훈 on 8/24/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.backgroundColor = .white
+        
+        let githubViewController = GithubViewController()
+        self.window?.rootViewController = UINavigationController(rootViewController: githubViewController)
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
+
